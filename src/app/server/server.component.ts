ba44687eff4aa7f121ece83bb86ae9d57206a5e1
Murad Zalov <mzalov@gmail.com>
2021-07-03 19:26:39 -0500

    First commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// @Component({}) is a TypeScript decorator
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
